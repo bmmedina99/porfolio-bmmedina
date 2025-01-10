d78818a4d0f830f88134da640ebb01a876ae49ab
MrUnknownDev <99305658+bmmedina99@users.noreply.github.com>
2025-01-10 05:01:45 +0100

    Marge changes from develop to production (#3)
    * feat(hero): :sparkles: Add Hero section to the portfolio
    
    * feat(navbar): :sparkles: Add desktop navbar
    
    * refactor(navbar): :recycle: Move navbar to the same level as hero
    
    * chore(public): :fire: Remove robots.txt to implement later with a package
    
    * feat(public): :camera_flash: Add new images and ico
    
    * style(navbar): :art: Update navbar styles and refactor code (#2)
    
    ---------
    
    Co-authored-by: bmmedina99 <contacto@bmmedina.dev>

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export const slugify = (text: string) =>
+  text
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/\p{M}/gu, '')
+    .replace(/[^a-z0-9]+/g, '-')
+
+export const capitalLetter = (text: string) =>
+  text.charAt(0).toUpperCase() + text.slice(1)
