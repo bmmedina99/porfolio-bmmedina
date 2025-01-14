1177075c82019429caa696711eef80eb117186e7
MrUnknownDev <99305658+bmmedina99@users.noreply.github.com>
2025-01-14 18:03:56 +0100

    chore(release): Deploy changes from develop to production (#10)
    * feat(portfolio): :sparkles: Add placeholder sections to rerserve space (#5)
    
    * style: :art: Update selection color and scrolbar styles (#6)
    
    * feat(hero): :sparkles: Add scroll down and scroll up buttons (#7)
    
    * fix(favicon): :bug: Resolve favicon visibility issue
    
    * refactor(navbar): :recycle: Migrate from Astro to React (Navbar.tsx) (#8)
    
    * feat(hero): :sparkles: Add blurred background animation behind hero
    
    * feat(icons): :sparkles: Rename component svgs to iconbase and add new SVG icon
    
    * feat(navbar): :sparkles: Add responsive design for navbar (#9)

---
+++ src/components/icons/Iconbase.tsx
@@ -0,0 +1,94 @@
+export const ChevronUp = () => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <title>Ir al inicio</title>
+      <path
+        stroke='none'
+        d='M0 0h24v24H0z'
+        fill='none'
+      />
+      <path d='M6 15l6 -6l6 6' />
+    </svg>
+  )
+}
+
+export const ChevronDown = () => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <title>Ir abajo</title>
+      <path
+        stroke='none'
+        d='M0 0h24v24H0z'
+        fill='none'
+      />
+      <path d='M6 9l6 6l6 -6' />
+    </svg>
+  )
+}
+
+export const Menu = () => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <title>Menu</title>
+      <path
+        stroke='none'
+        d='M0 0h24v24H0z'
+        fill='none'
+      />
+      <path d='M4 6l16 0' />
+      <path d='M4 12l16 0' />
+      <path d='M4 18l16 0' />
+    </svg>
+  )
+}
+
+export const Close = () => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <title>Cerrar</title>
+      <path
+        stroke='none'
+        d='M0 0h24v24H0z'
+        fill='none'
+      />
+      <path d='M18 6l-12 12' />
+      <path d='M6 6l12 12' />
+    </svg>
+  )
+}
