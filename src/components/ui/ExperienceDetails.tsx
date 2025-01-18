6041a799a475eca8da603f4dfa8829158dc89f28
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-01-19 00:55:05 +0100

    style(experience): :art: Minor design update in details section

--- src/components/ui/ExperienceDetails.tsx
+++ src/components/ui/ExperienceDetails.tsx
@@ -5,64 +5,62 @@ export default function ExperienceDetails() {
   return (
     <>
       {/* Experience 1 */}
-      <div className='bg-[#12042d]/60 border border-[#3f007d] rounded-lg p-4 mb-2'>
+      <div className='bg-[#12042d]/60 border border-[#3f007d] rounded-lg p-4 my-2'>
         <div className='flex justify-between item-center'>
           <h3 className='text-lg font-semibold tracking-wide'>
-            <span className='text-[#c6b8e1]'>Puesto</span> <span>&rarr;</span>{' '}
+            <span className='text-[#c084fc]'>Puesto</span> <span>&bull;</span>{' '}
             Empresa
           </h3>
           <ChevronUp />
         </div>
         <p className='text-sm text-[#cbd5e1]'>Fecha inicio - Presente</p>
       </div>
-      <div className='bg-[#3f007d]/60 border border-[#12042d] rounded-lg max-h-[600px] overflow-hidden space-y-3 p-4 mb-2'>
-        <div>
-          <p>&bull; Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p>&bull; Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p>&bull; Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <div className='bg-[#3f007d]/60 border border-[#12042d] rounded-lg max-h-[600px] overflow-hidden space-y-3 p-4'>
+        <p>&rarr; Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p>&rarr; Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p>&rarr; Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className='flex flex-wrap gap-2'>
+          <span className='bg-[#12042d]/40 text-[#c084fc] text-sm font-medium px-2.5 py-1 rounded-full border border-[#7042f8]'>Lorem</span>
+          <span className='bg-[#12042d]/40 text-[#c084fc] text-sm font-medium px-2.5 py-1 rounded-full border border-[#7042f8]'>Lorem</span>
+          <span className='bg-[#12042d]/40 text-[#c084fc] text-sm font-medium px-2.5 py-1 rounded-full border border-[#7042f8]'>Lorem</span>
+          <span className='bg-[#12042d]/40 text-[#c084fc] text-sm font-medium px-2.5 py-1 rounded-full border border-[#7042f8]'>Lorem</span>
+          <span className='bg-[#12042d]/40 text-[#c084fc] text-sm font-medium px-2.5 py-1 rounded-full border border-[#7042f8]'>Lorem</span>
         </div>
-        <div>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-        </div>
-        <p>
+        <p className='text-[#00ffaa]'>
           <span>🗺</span> Localicación
         </p>
-        <p>
+        <p className='text-[#22d2f0]'>
           <span>🌐</span>{' '}
           <a
             href='#web'
             target='_blank'
             rel='noopener noreferrer'
           >
-            Sitio web
+            https://sitio-web.com
           </a>
         </p>
       </div>
       {/* Experience 2 */}
-      <div className='bg-[#12042d]/60 border border-[#3f007d] rounded-lg p-4 mb-2'>
+      <div className='bg-[#12042d]/60 border border-[#3f007d] rounded-lg p-4 my-2'>
         <div className='flex justify-between item-center'>
           <h3 className='text-lg font-semibold tracking-wide'>
-            <span className='text-[#c6b8e1]'>Puesto</span> <span>&rarr;</span>{' '}
+            <span className='text-[#c084fc]'>Puesto</span> <span>&bull;</span>{' '}
             Empresa
           </h3>
-          <ChevronDown />
+          <ChevronUp />
         </div>
-        <p className='text-sm text-[#cbd5e1]'>Fecha inicio - Fecha fin</p>
+        <p className='text-sm text-[#cbd5e1]'>Fecha inicio - Presente</p>
       </div>
       {/* Experience 3 */}
-      <div className='bg-[#12042d]/60 border border-[#3f007d] rounded-lg p-4 mb-2'>
+      <div className='bg-[#12042d]/60 border border-[#3f007d] rounded-lg p-4 my-2'>
         <div className='flex justify-between item-center'>
           <h3 className='text-lg font-semibold tracking-wide'>
-            <span className='text-[#c6b8e1]'>Puesto</span> <span>&rarr;</span>{' '}
+            <span className='text-[#c084fc]'>Puesto</span> <span>&bull;</span>{' '}
             Empresa
           </h3>
-          <ChevronDown />
+          <ChevronUp />
         </div>
-        <p className='text-sm text-[#cbd5e1]'>Fecha inicio - Fecha fin</p>
+        <p className='text-sm text-[#cbd5e1]'>Fecha inicio - Presente</p>
       </div>
     </>
   )
