f9dd1217512b1e24e4ea7a49802dc71145b06c13
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-01-30 04:56:43 +0100

    refactor(icons): :recycle: Replace IconBase.tsx with a reusable Icon.tsx component

---
+++ src/components/ui/Icon.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  name: string
+  width?: number
+  height?: number
+}
+
+const Icon = ({ name, width = 24, height = 24 }: Props) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      fill='none'
+      stroke="currentColor"
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <use href={`assets/svg/icons.svg#${name}`} />
+    </svg>
+  )
+}
+
+export default Icon
