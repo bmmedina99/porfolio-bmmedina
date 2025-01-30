1ab78fdf52d3b0c183aa25d7dafb6d1e7f66b08a
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-01-30 01:47:08 +0100

    refactor(public): :truck: Reorganize public folder structure

--- src/components/icons/Iconbase.tsx
+++ src/components/icons/Iconbase.tsx
@@ -79,9 +79,8 @@ export const ChevronDown = () => {
       <path
         stroke='none'
         d='M0 0h24v24H0z'
-        fill='none'
       />
-      <path d='M6 9l6 6l6 -6' />
+      <path d='m6 9 6 6 6-6' />
     </svg>
   )
 }
