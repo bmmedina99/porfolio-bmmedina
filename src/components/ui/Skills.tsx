2b01f0066a970dd270ceea868bad59ee48f05ae0
MrUnknownDev <99305658+bmmedina99@users.noreply.github.com>
2025-02-15 16:28:40 +0100

    feat(icon): :sparkles: add technical skill icons  (#29)
    - Added new technical skills icons  
    - Implemented skill icons in the about section  
    - Corrected Node.js icon in skills.svg

---
+++ src/components/ui/Skills.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  name: string
+  width?: number
+  height?: number
+}
+
+const Skills = ({ name, width = 24, height = 24 }: Props) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      fill='none'
+    >
+      <use href={`assets/svg/skills.svg#${name}`} />
+    </svg>
+  )
+}
+
+export default Skills
