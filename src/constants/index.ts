79665bbe998573aeaad844e12885f39a45c741bc
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-03-05 23:47:58 +0100

    feat(footer): :sparkles: Add LinkedIn and Instagram icons

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const NAV_ITEMS = [
   { id: '01', label: 'experiencia' },
   { id: '02', label: 'proyectos' },
   { id: '03', label: 'sobre mí' },
-  { id: '04', label: 'contacto' },
+  { id: '04', label: 'contactáme' },
 ]
 
 export const EXPERIENCES: Experience[] = [
